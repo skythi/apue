d7585ce917fe7c0b057536dc1b8fa68220a0bc7c
skythi <opensail_ms@hotmail.com>
2020-11-23 12:44:20 +0800

    add

---
+++ ch10/捕捉两个用户定义的信号并打印信号编号/README.md
@@ -0,0 +1,1 @@
+![sig_usr_catch](https://skythi.oss-cn-beijing.aliyuncs.com/images/apue/ch10/sig_usr.png)
---
+++ ch10/捕捉两个用户定义的信号并打印信号编号/sig_usr.cpp
@@ -0,0 +1,24 @@
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+static void sig_usr(int);
+
+int main(void) {
+	if (signal(SIGUSR1, sig_usr) == SIG_ERR)
+		perror("can't catch SIGUSR1");
+	if (signal(SIGUSR2, sig_usr) == SIG_ERR)
+		perror("can't catch SIGUSR2");
+	for (;;) 
+		pause();
+}
+
+static void sig_usr(int signo) {
+	if (signo == SIGUSR1)
+		printf("received SIGUSR1\n");
+	else if (signo == SIGUSR2)
+		printf("received SIGUSR2\n");
+	else
+		perror("received signal %d\n");
+}
