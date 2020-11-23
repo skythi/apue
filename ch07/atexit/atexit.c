41b63f31a3e418510d2165c6ee71794cf6fadd3c
skythi <opensail_ms@hotmail.com>
2020-11-23 12:22:38 +0800

    add

---
+++ ch07/atexit/atexit.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+static void my_exit1(void);
+static void my_exit2(void);
+
+/*
+	atexit函数登记终止处理程序
+*/
+int main() {
+	if (atexit(my_exit2) != 0)
+		fprintf(stderr, "can't register my_exit2");
+	if(atexit(my_exit1) != 0)
+		fprintf(stderr, "can't register my_exit1");
+	if (atexit(my_exit1) != 0)
+		fprintf(stderr, "can't register my_exit1");
+	printf("main is done\n");
+	return(0);
+}
+
+static void my_exit1(void) {
+	printf("first exit handler\n");
+}
+
+static void my_exit2(void) {
+	printf("second exit handler\n");
+}
